ef61e8346bb7457349ec8920cc7be5431f503112
kataynoi <thait-rex@hotmail.com>
2017-05-18 07:02:39 +0700

    add Dashboard

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -15,9 +15,9 @@ class Pages extends CI_Controller
     public function index()
     {
         $data['percent_audit'] = number_format($this->reports->get_percent_audit(), 2);
-        $data['percent_audit_true'] = number_format($this->reports->get_percent_audit_true(), 2);;
-        $data['percent_audit_icd10'] = '0';
-        $data['percent_audit_icd10_true'] = '0';
+        $data['percent_audit_true'] = number_format($this->reports->get_percent_audit_true(), 2);
+        $data['percent_audit_icd10'] = number_format($this->reports->get_percent_audit_icd10(), 2);;
+        $data['percent_audit_icd10_true'] = number_format($this->reports->get_percent_audit_icd10_true(), 2);;
         $this->layout->view('pages/index_view', $data);
         $this->load->view('download/index_view');
     }
--- application/models/Reports_model.php
+++ application/models/Reports_model.php
@@ -28,6 +28,26 @@ class Reports_model extends CI_Model {
         return $rs->percent_audit_true;
     }
 
+    public function get_percent_audit_icd10(){
+        $rs = $this->db
+            ->select('SUM(IF(AUDIT_ICD10 IS NOT NULL,1,0))*100/COUNT(*) as percent_audit_icd10')
+            //->where('a.note','1/2560')
+            //->join('audit b','a.id=b.data_audit_id','left')
+            ->get('diagnosis_opd a')
+            ->row();
+        return $rs->percent_audit_icd10;
+    }
+    public function get_percent_audit_icd10_true(){
+        $rs = $this->db
+           // ->select('SUM(IF(b.percent >=75,1,0))*100/SUM(IF(b.percent IS NOT NULL,1,0)) as percent_audit_true')
+            ->select('SUM(IF(AUDIT_ICD10="Y",1,0))*100/SUM(IF(AUDIT_ICD10 IS NOT NULL,1,0)) as percent_audit_icd10_true')
+            //->where('a.note','1/2560')
+            //->join('audit b','a.id=b.data_audit_id','left')
+            ->get('diagnosis_opd a')
+            ->row();
+        return $rs->percent_audit_icd10_true;
+    }
+
 }
 
 /* End of file patient_model.php */
